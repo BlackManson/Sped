c86e36198ad04840c078716d9fd329df45b4dd8a
Łukasz Kołodziej <lukasz.kolodziej@student.up.krakow.pl>
2021-01-30 13:33:47 +0100

    Create emulator run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Start the emulator
-$ANDROID_SDK/emulator/emulator -avd Nexus_S_API_29 &
+/Users/lukaszkolodziej/Library/Android/sdk/emulator/emulator -avd Nexus_S_API_29 &
 EMULATOR_PID=$!
 
 # Wait for Android to finish booting
-WAIT_CMD="$ANDROID_SDK/platform-tools/adb wait-for-device shell getprop init.svc.bootanim"
+WAIT_CMD="/Users/lukaszkolodziej/Library/Android/sdk/platform-tools/adb wait-for-device shell getprop init.svc.bootanim"
 until $WAIT_CMD | grep -m 1 stopped; do
   echo "Waiting..."
   sleep 1
@@ -15,8 +15,8 @@ done
 #$ANDROID_SDK/platform-tools/adb shell input keyevent 82
 
 # Clear and capture logcat
-$ANDROID_SDK/platform-tools/adb logcat -c
-$ANDROID_SDK/platform-tools/adb logcat > build/logcat.log &
+/Users/lukaszkolodziej/Library/Android/sdk/platform-tools/adb logcat -c
+/Users/lukaszkolodziej/Library/Android/sdk/platform-tools/adb logcat > build/logcat.log &
 LOGCAT_PID=$!
 
 # Run the tests
